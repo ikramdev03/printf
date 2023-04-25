31da546d5a3488df8d14376beca7b9b16f56f069
ikramdev03 <izmerikram03@gmail.com>
2023-04-24 20:54:52 -0700

    modify fprintf

--- _printf.c
+++ _printf.c
@@ -11,19 +11,6 @@ int	ft_checkmate(const char c, va_list ap)
 		count += ft_putchar(va_arg(ap, int));
 	else if (c == 's')
 		count += ft_putstr(va_arg(ap, const char *));
-	else if (c == 'p')
-	{
-		count += ft_putstr("0x");
-		count += ft_hexadecimal(va_arg(ap, unsigned long));
-	}
-	else if (c == 'd' || c == 'i')
-		count += ft_putnbr(va_arg(ap, int));
-	else if (c == 'u')
-		count += ft_putunbr(va_arg(ap, unsigned int));
-	else if (c == 'x')
-		count += ft_hex(va_arg(ap, unsigned int), 0);
-	else if (c == 'X')
-		count += ft_hex(va_arg(ap, unsigned int), 1);
 	return (count);
 }
 
